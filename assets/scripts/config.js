45bcb1f70b87bb22cb19a1d839f1cbdb9aeecb76
dhdonaldha1 <donaldhas.email@gmail.com>
2020-09-24 12:44:59 -0400

    updating production url updated config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://intense-hamlet-48353.herokuapp.com/',
+  production: 'https://intense-hamlet-48353.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
